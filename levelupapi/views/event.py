bafb53ee5bf1b4f16a31e38bc499492e287cb01e
Whitley Beers <whitleybeers@gmail.com>
2023-05-30 21:18:15 -0500

    added ability for a gamer to join and leave an event, tested in postman

--- levelupapi/views/event.py
+++ levelupapi/views/event.py
@@ -3,7 +3,8 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from levelupapi.models import Event, Game, Gamer
+from levelupapi.models import Event, Game, Gamer, EventGamer
+from rest_framework.decorators import action
 
 
 class EventView(ViewSet):
@@ -77,6 +78,31 @@ class EventView(ViewSet):
         """
         event = Event.objects.get(pk=pk)
         event.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+    @action(methods=['post'], detail=True)
+    def signup(self, request, pk):
+        """Post request for a user to sign up for an event"""
+
+        gamer = Gamer.objects.get(uid=request.data["userId"])
+        event = Event.objects.get(pk=pk)
+        attendee = EventGamer.objects.create(
+            gamer=gamer,
+            event=event
+        )
+        return Response({'message': 'Gamer added'}, status=status.HTTP_201_CREATED)
+
+    @action(methods=['delete'], detail=True)
+    def leave(self, request, pk):
+        """Delete request for a user to leave an event"""
+
+        gamer = Gamer.objects.get(uid=request.data["userId"])
+        event = Event.objects.get(pk=pk)
+        event_gamer = EventGamer.objects.get(
+            event_id = event.id,
+            gamer_id = gamer.id
+        )
+        event_gamer.delete()
         return Response(None, status=status.HTTP_204_NO_CONTENT)
 
 
